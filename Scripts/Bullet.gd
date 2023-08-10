23d5b6202b6440e7dfaa64fcf2bd74cb528d00db
Bunny Blake <discolingua@gmail.com>
2023-08-09 22:43:43 -0400

    bullets correct speed and they despawn

--- Scripts/Bullet.gd
+++ Scripts/Bullet.gd
@@ -2,7 +2,7 @@ extends Area2D
 
 class_name Bullet
 
-const SPEED = 50
+const SPEED = 10
 
 var velocity : Vector2 = Vector2(0,SPEED * -1)
 
@@ -10,4 +10,11 @@ func _ready():
 	print("bullet ready")
 
 func _physics_process(_delta) -> void:
-	pass
+	position += velocity
+
+
+# despawn bullet when it's gone offscreen
+func _on_BulletLifeTimer_timeout():
+	print("click")
+	if ( position.y < 0 ):
+		queue_free()
